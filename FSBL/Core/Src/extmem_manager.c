3db32cf60424ebeac744621f7a90709669dc346b
Foahh <160589889+Foahh@users.noreply.github.com>
2026-01-21 05:04:55 +0800

    refactor(FSBL): removing CubeMX generated code

--- FSBL/Core/Src/extmem_manager.c
+++ FSBL/Core/Src/extmem_manager.c
@@ -1,55 +1,29 @@
-/* USER CODE BEGIN Header */
 /**
-  ******************************************************************************
-  * @file           : extmem_manager.c
-  * @version        : 1.0.0
-  * @brief          : This file implements the extmem configuration
-  ******************************************************************************
-  * @attention
-  *
-  * Copyright (c) 2025 STMicroelectronics.
-  * All rights reserved.
-  *
-  * This software is licensed under terms that can be found in the LICENSE file
-  * in the root directory of this software component.
-  * If no LICENSE file comes with this software, it is provided AS-IS.
-  *
-  ******************************************************************************
-  */
-/* USER CODE END Header */
+ ******************************************************************************
+ * @file           : extmem_manager.c
+ * @version        : 1.0.0
+ * @brief          : This file implements the extmem configuration
+ ******************************************************************************
+ * @attention
+ *
+ * Copyright (c) 2025 STMicroelectronics.
+ * All rights reserved.
+ *
+ * This software is licensed under terms that can be found in the LICENSE file
+ * in the root directory of this software component.
+ * If no LICENSE file comes with this software, it is provided AS-IS.
+ *
+ ******************************************************************************
+ */
 
 /* Includes ------------------------------------------------------------------*/
 #include "extmem_manager.h"
-#include <string.h>
+#include "memory.h"
 
-/* USER CODE BEGIN Includes */
-
-/* USER CODE END Includes */
-
-/* USER CODE BEGIN PV */
-/* Private variables ---------------------------------------------------------*/
-
-/* USER CODE END PV */
-
-/* USER CODE BEGIN PFP */
-/* Private function prototypes -----------------------------------------------*/
-
-/* USER CODE END PFP */
-
-/*
- * -- Insert your variables declaration here --
- */
-/* USER CODE BEGIN 0 */
 #define HEADER_V2_3_IMG_SIZE_OFFSET 108
 #define HEADER_V2_3_SIZE 1024
-/* USER CODE END 0 */
 
-/*
- * -- Insert your external function declaration here --
- */
-/* USER CODE BEGIN 1 */
-uint32_t BOOT_GetApplicationSize(uint32_t img_addr)
-{
+uint32_t BOOT_GetApplicationSize(uint32_t img_addr) {
   uint32_t img_size;
 
   img_size = (*(uint32_t *)(img_addr + HEADER_V2_3_IMG_SIZE_OFFSET));
@@ -57,30 +31,20 @@ uint32_t BOOT_GetApplicationSize(uint32_t img_addr)
 
   return img_size;
 }
-/* USER CODE END 1 */
 
 /**
-  * Init External memory manager
-  * @retval None
-  */
-void MX_EXTMEM_MANAGER_Init(void)
-{
-
-  /* USER CODE BEGIN MX_EXTMEM_Init_PreTreatment */
-
-  /* USER CODE END MX_EXTMEM_Init_PreTreatment */
+ * Init External memory manager
+ * @retval None
+ */
+void EXTMEM_MANAGER_Init(void) {
 
   /* Initialization of the memory parameters */
   memset(extmem_list_config, 0x0, sizeof(extmem_list_config));
 
   /* EXTMEMORY_1 */
   extmem_list_config[0].MemType = EXTMEM_NOR_SFDP;
-  extmem_list_config[0].Handle = (void*)&hxspi2;
+  extmem_list_config[0].Handle = (void *)&hxspi2;
   extmem_list_config[0].ConfigType = EXTMEM_LINK_CONFIG_8LINES;
 
   EXTMEM_Init(EXTMEMORY_1, HAL_RCCEx_GetPeriphCLKFreq(RCC_PERIPHCLK_XSPI2));
-
-  /* USER CODE BEGIN MX_EXTMEM_Init_PostTreatment */
-
-  /* USER CODE END MX_EXTMEM_Init_PostTreatment */
 }
--- FSBL/Core/Src/main.c
+++ FSBL/Core/Src/main.c
@@ -1,151 +1,101 @@
-/* USER CODE BEGIN Header */
 /**
-  ******************************************************************************
-  * @file           : main.c
-  * @brief          : Main program body
-  ******************************************************************************
-  * @attention
-  *
-  * Copyright (c) 2025 STMicroelectronics.
-  * All rights reserved.
-  *
-  * This software is licensed under terms that can be found in the LICENSE file
-  * in the root directory of this software component.
-  * If no LICENSE file comes with this software, it is provided AS-IS.
-  *
-  ******************************************************************************
-  */
-/* USER CODE END Header */
-/* Includes ------------------------------------------------------------------*/
+ ******************************************************************************
+ * @file           : main.c
+ * @author         : Long Liangmao
+ * @brief          : Main program body
+ ******************************************************************************
+ * @attention
+ *
+ * Copyright (c) 2026 Long Liangmao.
+ * All rights reserved.
+ *
+ * This software is licensed under terms that can be found in the LICENSE file
+ * in the root directory of this software component.
+ * If no LICENSE file comes with this software, it is provided AS-IS.
+ *
+ ******************************************************************************
+ */
+
 #include "main.h"
 #include "extmem_manager.h"
-#include "xspi.h"
-#include "xspim.h"
-#include "gpio.h"
-
-/* Private includes ----------------------------------------------------------*/
-/* USER CODE BEGIN Includes */
+#include "stm32_extmem_conf.h"
 #include "stm32n6570_discovery.h"
-/* USER CODE END Includes */
+#include <stdint.h>
 
-/* Private typedef -----------------------------------------------------------*/
-/* USER CODE BEGIN PTD */
+/*
+ * Exported variables
+ */
+XSPI_HandleTypeDef hxspi2;
 
-/* USER CODE END PTD */
-
-/* Private define ------------------------------------------------------------*/
-/* USER CODE BEGIN PD */
-
-/* USER CODE END PD */
-
-/* Private macro -------------------------------------------------------------*/
-/* USER CODE BEGIN PM */
-
-/* USER CODE END PM */
-
-/* Private variables ---------------------------------------------------------*/
-
-/* USER CODE BEGIN PV */
-/* USER CODE END PV */
-
-/* Private function prototypes -----------------------------------------------*/
-void SystemClock_Config(void);
-/* USER CODE BEGIN PFP */
-static int32_t OTP_Config(void);
-static void SMPS_Config(void);
-static void LED_Config(void);
+/* ============================================================================
+ * Forward Declarations
+ * ============================================================================ */
+static void XSPI_Power_Config(void);
 static void XSPI_Config(void);
-/* USER CODE END PFP */
+static void LED_Config(void);
+static void SystemClock_Config(void);
+static void OTP_Config(void);
+static void GPIO_Config(void);
 
-/* Private user code ---------------------------------------------------------*/
-/* USER CODE BEGIN 0 */
+/* ============================================================================
+ * OTP Configuration Constants
+ * ============================================================================ */
+#define BSEC_HW_CONFIG_ID 124U
+#define BSEC_HWS_HSLV_VDDIO3 (1U << 15)
+#define BSEC_HWS_HSLV_VDDIO2 (1U << 16)
+#define BSEC_HWS_HSLV_MASK (BSEC_HWS_HSLV_VDDIO3 | BSEC_HWS_HSLV_VDDIO2)
 
-/* USER CODE END 0 */
-
-/**
-  * @brief  The application entry point.
-  * @retval int
-  */
-int main(void)
-{
-
-  /* USER CODE BEGIN 1 */
-  /* USER CODE END 1 */
-
-  /* MCU Configuration--------------------------------------------------------*/
+/* ============================================================================
+ * Main Function
+ * ============================================================================ */
+int main(void) {
   HAL_Init();
 
-  /* USER CODE BEGIN Init */
-  SMPS_Config();
-  /* USER CODE END Init */
-
-  /* Configure the system clock */
   SystemClock_Config();
 
-  /* USER CODE BEGIN SysInit */
-
-  /* Set OTP fuses for XSPI IO pins speed optimization */
-  if(OTP_Config() != 0){
-    Error_Handler();
-  }
-
-  XSPI_Config();
-  /* USER CODE END SysInit */
-
-  /* Initialize all configured peripherals */
-  MX_GPIO_Init();
-  MX_XSPI2_Init();
-  MX_XSPI1_Init();
-  MX_EXTMEM_MANAGER_Init();
-  /* USER CODE BEGIN 2 */
   LED_Config();
 
-  // TODO: ThreadX should not be generated as part of this FSBL project.
-  // This appears to be an STM32CubeMX/STM32Cube_FW_N6 v1.3.0 generation bug.
+  GPIO_Config();
 
-  /* USER CODE END 2 */
+  OTP_Config();
 
-  /* We should never get here as control is now taken by the scheduler */
+  XSPI_Power_Config();
+  XSPI_Config();
 
-  /* Launch the application */
-  if (BOOT_OK != BOOT_Application())
-  {
+  EXTMEM_MANAGER_Init();
+
+  if (BOOT_Application() != BOOT_OK) {
     Error_Handler();
   }
-  /* Infinite loop */
-  /* USER CODE BEGIN WHILE */
-  while (1)
-  {
-    /* USER CODE END WHILE */
 
-    /* USER CODE BEGIN 3 */
+  for (;;) {
+    // do nothing
   }
-  /* USER CODE END 3 */
 }
-/* USER CODE BEGIN CLK 1 */
-/* USER CODE END CLK 1 */
+
+/* ============================================================================
+ * Private Functions
+ * ============================================================================ */
 
 /**
-  * @brief System Clock Configuration
-  * @retval None
-  */
-void SystemClock_Config(void)
-{
+ * @brief System Clock Configuration
+ * @retval None
+ */
+
+static void SystemClock_Config(void) {
   RCC_PeriphCLKInitTypeDef PeriphClkInitStruct = {0};
   RCC_OscInitTypeDef RCC_OscInitStruct = {0};
   RCC_ClkInitTypeDef RCC_ClkInitStruct = {0};
 
   /** Configure the System Power Supply
-  */
-  if (HAL_PWREx_ConfigSupply(PWR_EXTERNAL_SOURCE_SUPPLY) != HAL_OK)
-  {
+   */
+  if (HAL_PWREx_ConfigSupply(PWR_EXTERNAL_SOURCE_SUPPLY) != HAL_OK) {
     Error_Handler();
   }
 
   /** Configure the main internal regulator output voltage
-  */
-  if (HAL_PWREx_ControlVoltageScaling(PWR_REGULATOR_VOLTAGE_SCALE0) != HAL_OK)
-  {
+   */
+  if (HAL_PWREx_ControlVoltageScaling(PWR_REGULATOR_VOLTAGE_SCALE0) != HAL_OK) {
     Error_Handler();
   }
 
@@ -158,17 +108,15 @@ void SystemClock_Config(void)
   RCC_OscInitStruct.PLL2.PLLState = RCC_PLL_NONE;
   RCC_OscInitStruct.PLL3.PLLState = RCC_PLL_NONE;
   RCC_OscInitStruct.PLL4.PLLState = RCC_PLL_NONE;
-  if (HAL_RCC_OscConfig(&RCC_OscInitStruct) != HAL_OK)
-  {
+  if (HAL_RCC_OscConfig(&RCC_OscInitStruct) != HAL_OK) {
     Error_Handler();
   }
 
   /** Initializes TIMPRE when TIM is used as Systick Clock Source
-  */
+   */
   PeriphClkInitStruct.PeriphClockSelection = RCC_PERIPHCLK_TIM;
   PeriphClkInitStruct.TIMPresSelection = RCC_TIMPRES_DIV1;
-  if (HAL_RCCEx_PeriphCLKConfig(&PeriphClkInitStruct) != HAL_OK)
-  {
+  if (HAL_RCCEx_PeriphCLKConfig(&PeriphClkInitStruct) != HAL_OK) {
     Error_Handler();
   }
 
@@ -177,21 +125,19 @@ void SystemClock_Config(void)
   */
   HAL_RCC_GetClockConfig(&RCC_ClkInitStruct);
   if ((RCC_ClkInitStruct.CPUCLKSource == RCC_CPUCLKSOURCE_IC1) ||
-     (RCC_ClkInitStruct.SYSCLKSource == RCC_SYSCLKSOURCE_IC2_IC6_IC11))
-  {
+      (RCC_ClkInitStruct.SYSCLKSource == RCC_SYSCLKSOURCE_IC2_IC6_IC11)) {
     RCC_ClkInitStruct.ClockType = (RCC_CLOCKTYPE_CPUCLK | RCC_CLOCKTYPE_SYSCLK);
     RCC_ClkInitStruct.CPUCLKSource = RCC_CPUCLKSOURCE_HSI;
     RCC_ClkInitStruct.SYSCLKSource = RCC_SYSCLKSOURCE_HSI;
-    if (HAL_RCC_ClockConfig(&RCC_ClkInitStruct) != HAL_OK)
-    {
+    if (HAL_RCC_ClockConfig(&RCC_ClkInitStruct) != HAL_OK) {
       /* Initialization Error */
       Error_Handler();
     }
   }
 
   /** Initializes the RCC Oscillators according to the specified parameters
-  * in the RCC_OscInitTypeDef structure.
-  */
+   * in the RCC_OscInitTypeDef structure.
+   */
   RCC_OscInitStruct.OscillatorType = RCC_OSCILLATORTYPE_NONE;
   RCC_OscInitStruct.PLL1.PLLState = RCC_PLL_ON;
   RCC_OscInitStruct.PLL1.PLLSource = RCC_PLLSOURCE_HSI;
@@ -222,17 +168,13 @@ void SystemClock_Config(void)
   RCC_OscInitStruct.PLL4.PLLP1 = 6;
   RCC_OscInitStruct.PLL4.PLLP2 = 6;
 
-  if (HAL_RCC_OscConfig(&RCC_OscInitStruct) != HAL_OK)
-  {
+  if (HAL_RCC_OscConfig(&RCC_OscInitStruct) != HAL_OK) {
     Error_Handler();
   }
 
   /** Initializes the CPU, AHB and APB buses clocks
-  */
-  RCC_ClkInitStruct.ClockType = RCC_CLOCKTYPE_CPUCLK|RCC_CLOCKTYPE_HCLK
-                              |RCC_CLOCKTYPE_SYSCLK|RCC_CLOCKTYPE_PCLK1
-                              |RCC_CLOCKTYPE_PCLK2|RCC_CLOCKTYPE_PCLK5
-                              |RCC_CLOCKTYPE_PCLK4;
+   */
+  RCC_ClkInitStruct.ClockType = RCC_CLOCKTYPE_CPUCLK | RCC_CLOCKTYPE_HCLK | RCC_CLOCKTYPE_SYSCLK | RCC_CLOCKTYPE_PCLK1 | RCC_CLOCKTYPE_PCLK2 | RCC_CLOCKTYPE_PCLK5 | RCC_CLOCKTYPE_PCLK4;
   RCC_ClkInitStruct.CPUCLKSource = RCC_CPUCLKSOURCE_IC1;
   RCC_ClkInitStruct.SYSCLKSource = RCC_SYSCLKSOURCE_IC2_IC6_IC11;
   RCC_ClkInitStruct.AHBCLKDivider = RCC_HCLK_DIV2;
@@ -249,20 +191,47 @@ void SystemClock_Config(void)
   RCC_ClkInitStruct.IC11Selection.ClockSelection = RCC_ICCLKSOURCE_PLL3;
   RCC_ClkInitStruct.IC11Selection.ClockDivider = 1;
 
-  if (HAL_RCC_ClockConfig(&RCC_ClkInitStruct) != HAL_OK)
-  {
+  if (HAL_RCC_ClockConfig(&RCC_ClkInitStruct) != HAL_OK) {
     Error_Handler();
   }
 }
 
-/* USER CODE BEGIN 4 */
-
-static void XSPI_Config(void) {
+static void XSPI_Power_Config(void) {
   __HAL_RCC_PWR_CLK_ENABLE();
   HAL_PWREx_EnableVddIO3();
   HAL_PWREx_ConfigVddIORange(PWR_VDDIO3, PWR_VDDIO_RANGE_1V8);
   HAL_PWREx_EnableVddIO2();
   HAL_PWREx_ConfigVddIORange(PWR_VDDIO2, PWR_VDDIO_RANGE_1V8);
+}
+
+static void XSPI_Config(void) {
+  XSPIM_CfgTypeDef sXspiManagerCfg = {0};
+
+  hxspi2.Instance = XSPI2;
+  hxspi2.Init.FifoThresholdByte = 4;
+  hxspi2.Init.MemoryMode = HAL_XSPI_SINGLE_MEM;
+  hxspi2.Init.MemoryType = HAL_XSPI_MEMTYPE_MACRONIX;
+  hxspi2.Init.MemorySize = HAL_XSPI_SIZE_1GB;
+  hxspi2.Init.ChipSelectHighTimeCycle = 1;
+  hxspi2.Init.FreeRunningClock = HAL_XSPI_FREERUNCLK_DISABLE;
+  hxspi2.Init.ClockMode = HAL_XSPI_CLOCK_MODE_0;
+  hxspi2.Init.WrapSize = HAL_XSPI_WRAP_NOT_SUPPORTED;
+  hxspi2.Init.ClockPrescaler = 0;
+  hxspi2.Init.SampleShifting = HAL_XSPI_SAMPLE_SHIFT_NONE;
+  hxspi2.Init.DelayHoldQuarterCycle = HAL_XSPI_DHQC_ENABLE;
+  hxspi2.Init.ChipSelectBoundary = HAL_XSPI_BONDARYOF_NONE;
+  hxspi2.Init.MaxTran = 0;
+  hxspi2.Init.Refresh = 0;
+  hxspi2.Init.MemorySelect = HAL_XSPI_CSSEL_NCS1;
+  if (HAL_XSPI_Init(&hxspi2) != HAL_OK) {
+    Error_Handler();
+  }
+  sXspiManagerCfg.nCSOverride = HAL_XSPI_CSSEL_OVR_NCS1;
+  sXspiManagerCfg.IOPort = HAL_XSPIM_IOPORT_2;
+  sXspiManagerCfg.Req2AckTime = 1;
+  if (HAL_XSPIM_Config(&hxspi2, &sXspiManagerCfg, HAL_XSPI_TIMEOUT_DEFAULT_VALUE) != HAL_OK) {
+    Error_Handler();
+  }
 }
 
 static void LED_Config(void) {
@@ -272,114 +241,158 @@ static void LED_Config(void) {
   BSP_LED_Off(LED_RED);
 }
 
-static void SMPS_Config(void) {
-  BSP_SMPS_Init(SMPS_VOLTAGE_OVERDRIVE);
-  HAL_Delay(2); /* Assuming Voltage Ramp Speed of 1mV/us --> 100mV increase takes 100us */
+static void GPIO_Config(void) {
+  __HAL_RCC_GPIOP_CLK_ENABLE();
+  __HAL_RCC_GPIOO_CLK_ENABLE();
+  __HAL_RCC_GPION_CLK_ENABLE();
 }
 
 /**
-  * @brief  User OTP fuse Configuration
-  *         The User Option Bytes are configured as follows :
-  *            VDDIO_HSLV = 1 (enable the configuration of pads below 2.5V,
-  *                            I/O speed otpmization at low-voltage allowed)
-  *            XSPI1_HSLV = 1 (enable I/O XSPIM Port 1 high-speed option)
-  *            XSPI2_HSLV = 1 (enable I/O XSPIM Port 2 high-speed option)
-  *            Other User Option Bytes remain unchanged
-  * @retval None
-  */
-static int32_t OTP_Config(void)
-{
-  #define BSEC_HW_CONFIG_ID        124U
-  #define BSEC_HWS_HSLV_VDDIO3     (1U<<15)
-  #define BSEC_HWS_HSLV_VDDIO2     (1U<<16)
-
-  int32_t retr = 0;
-
+ * @brief  User OTP fuse Configuration
+ *         The User Option Bytes are configured as follows :
+ *            VDDIO_HSLV = 1 (enable the configuration of pads below 2.5V,
+ *                            I/O speed optimization at low-voltage allowed)
+ *            XSPI1_HSLV = 1 (enable I/O XSPIM Port 1 high-speed option)
+ *            XSPI2_HSLV = 1 (enable I/O XSPIM Port 2 high-speed option)
+ *            Other User Option Bytes remain unchanged
+ * @retval None
+ */
+static void OTP_Config(void) {
+  /* Enable required clocks */
   __HAL_RCC_SYSCFG_CLK_ENABLE();
   __HAL_RCC_BSEC_CLK_ENABLE();
 
+  /* Initialize BSEC handler */
   BSEC_HandleTypeDef sBsecHandler;
   sBsecHandler.Instance = BSEC;
 
-  /* Read current value of fuse */
+  /* Read current fuse value */
   uint32_t fuse_id = BSEC_HW_CONFIG_ID;
-  uint32_t data;
-  if (HAL_BSEC_OTP_Read(&sBsecHandler, fuse_id, &data) == HAL_OK)
-  {
-    /* Check if bit has already been set */
-    uint32_t bit_mask = BSEC_HWS_HSLV_VDDIO3 | BSEC_HWS_HSLV_VDDIO2;
-    if ((data & bit_mask) != bit_mask)
-    {
-      data |= bit_mask;
-      /* Bitwise programming of lower bits */
-      if (HAL_BSEC_OTP_Program(&sBsecHandler, fuse_id, data, HAL_BSEC_NORMAL_PROG) == HAL_OK)
-      {
-        /* Read lower bits to verify the correct programming */
-        if (HAL_BSEC_OTP_Read(&sBsecHandler, fuse_id, &data) == HAL_OK)
-        {
-          if ((data & bit_mask) != bit_mask)
-          {
-            /* Error : Fuse programming not taken in account */
-            retr = -1;
-          }
-        }
-        else
-        {
-          /* Error : Fuse read unsuccessful */
-          retr = -2;
-        }
-      }
-      else
-      {
-        /* Error : Fuse programming unsuccessful */
-        retr = -3;
-      }
-    }
+  uint32_t fuse_data;
+
+  if (HAL_BSEC_OTP_Read(&sBsecHandler, fuse_id, &fuse_data) != HAL_OK) {
+    Error_Handler();
+    return;
   }
-  else
-  {
-    /* Error  : Fuse read unsuccessful */
-    retr = -4;
+
+  /* Check if HSLV bits are already configured */
+  if ((fuse_data & BSEC_HWS_HSLV_MASK) == BSEC_HWS_HSLV_MASK) {
+    /* Configuration already set, no action needed */
+    return;
   }
-  return retr;
+
+  /* Set HSLV bits */
+  fuse_data |= BSEC_HWS_HSLV_MASK;
+
+  /* Program the fuse */
+  if (HAL_BSEC_OTP_Program(&sBsecHandler, fuse_id, fuse_data, HAL_BSEC_NORMAL_PROG) != HAL_OK) {
+    Error_Handler();
+    return;
+  }
+
+  /* Verify programming by reading back */
+  uint32_t verify_data;
+  if (HAL_BSEC_OTP_Read(&sBsecHandler, fuse_id, &verify_data) != HAL_OK) {
+    Error_Handler();
+    return;
+  }
+
+  /* Check if programming was successful */
+  if ((verify_data & BSEC_HWS_HSLV_MASK) != BSEC_HWS_HSLV_MASK) {
+    Error_Handler();
+    return;
+  }
 }
 
-/* USER CODE END 4 */
+/* ============================================================================
+ * HAL MSP Functions
+ * ============================================================================ */
 
-/**
-  * @brief  This function is executed in case of error occurrence.
-  * @retval None
-  */
-void Error_Handler(void)
-{
-  /* USER CODE BEGIN Error_Handler_Debug */
+void HAL_MspInit(void) {
+  HAL_PWREx_EnableVddIO2();
+  HAL_PWREx_EnableVddIO3();
+  HAL_PWREx_EnableVddIO4();
+  HAL_PWREx_EnableVddIO5();
+}
+
+void HAL_XSPI_MspInit(XSPI_HandleTypeDef *xspiHandle) {
+
+  GPIO_InitTypeDef GPIO_InitStruct = {0};
+  RCC_PeriphCLKInitTypeDef PeriphClkInitStruct = {0};
+  if (xspiHandle->Instance == XSPI2) {
+    /** Initializes the peripherals clock */
+    PeriphClkInitStruct.PeriphClockSelection = RCC_PERIPHCLK_XSPI2;
+    PeriphClkInitStruct.Xspi2ClockSelection = RCC_XSPI2CLKSOURCE_HCLK;
+    if (HAL_RCCEx_PeriphCLKConfig(&PeriphClkInitStruct) != HAL_OK) {
+      Error_Handler();
+    }
+
+    /* XSPI2 clock enable */
+    __HAL_RCC_XSPIM_CLK_ENABLE();
+    __HAL_RCC_XSPI2_CLK_ENABLE();
+
+    __HAL_RCC_GPION_CLK_ENABLE();
+    /**XSPI2 GPIO Configuration
+    PN4     ------> XSPIM_P2_IO2
+    PN6     ------> XSPIM_P2_CLK
+    PN8     ------> XSPIM_P2_IO4
+    PN0     ------> XSPIM_P2_DQS0
+    PN3     ------> XSPIM_P2_IO1
+    PN5     ------> XSPIM_P2_IO3
+    PN1     ------> XSPIM_P2_NCS1
+    PN9     ------> XSPIM_P2_IO5
+    PN2     ------> XSPIM_P2_IO0
+    PN10     ------> XSPIM_P2_IO6
+    PN11     ------> XSPIM_P2_IO7
+    */
+    GPIO_InitStruct.Pin = GPIO_PIN_4 | GPIO_PIN_6 | GPIO_PIN_8 | GPIO_PIN_0 | GPIO_PIN_3 | GPIO_PIN_5 | GPIO_PIN_1 | GPIO_PIN_9 | GPIO_PIN_2 | GPIO_PIN_10 | GPIO_PIN_11;
+    GPIO_InitStruct.Mode = GPIO_MODE_AF_PP;
+    GPIO_InitStruct.Pull = GPIO_NOPULL;
+    GPIO_InitStruct.Speed = GPIO_SPEED_FREQ_VERY_HIGH;
+    GPIO_InitStruct.Alternate = GPIO_AF9_XSPIM_P2;
+    HAL_GPIO_Init(GPION, &GPIO_InitStruct);
+  }
+}
+
+void HAL_XSPI_MspDeInit(XSPI_HandleTypeDef *xspiHandle) {
+
+  if (xspiHandle->Instance == XSPI2) {
+    /* Peripheral clock disable */
+    __HAL_RCC_XSPIM_CLK_DISABLE();
+    __HAL_RCC_XSPI2_CLK_DISABLE();
+
+    /**XSPI2 GPIO Configuration
+    PN4     ------> XSPIM_P2_IO2
+    PN6     ------> XSPIM_P2_CLK
+    PN8     ------> XSPIM_P2_IO4
+    PN0     ------> XSPIM_P2_DQS0
+    PN3     ------> XSPIM_P2_IO1
+    PN5     ------> XSPIM_P2_IO3
+    PN1     ------> XSPIM_P2_NCS1
+    PN9     ------> XSPIM_P2_IO5
+    PN2     ------> XSPIM_P2_IO0
+    PN10     ------> XSPIM_P2_IO6
+    PN11     ------> XSPIM_P2_IO7
+    */
+    HAL_GPIO_DeInit(GPION, GPIO_PIN_4 | GPIO_PIN_6 | GPIO_PIN_8 | GPIO_PIN_0 | GPIO_PIN_3 | GPIO_PIN_5 | GPIO_PIN_1 | GPIO_PIN_9 | GPIO_PIN_2 | GPIO_PIN_10 | GPIO_PIN_11);
+  }
+}
+
+/* ============================================================================
+ * Debug Functions
+ * ============================================================================ */
+
+void Error_Handler(void) {
   BSP_LED_Off(LED_GREEN);
   BSP_LED_Off(LED_RED);
 
   /* Disable interrupts to prevent further execution */
   __disable_irq();
 
-  while (1) {
+  for (;;) {
     BSP_LED_Toggle(LED_GREEN);
     for (volatile uint32_t i = 0; i < 4000000; i++) {
       __NOP();
     }
   }
-  /* USER CODE END Error_Handler_Debug */
 }
-#ifdef USE_FULL_ASSERT
-/**
-  * @brief  Reports the name of the source file and the source line number
-  *         where the assert_param error has occurred.
-  * @param  file: pointer to the source file name
-  * @param  line: assert_param error line source number
-  * @retval None
-  */
-void assert_failed(uint8_t *file, uint32_t line)
-{
-  /* USER CODE BEGIN 6 */
-  /* User can add his own implementation to report the file name and line number,
-     ex: printf("Wrong parameters value: file %s on line %d\r\n", file, line) */
-  /* USER CODE END 6 */
-}
-#endif /* USE_FULL_ASSERT */
