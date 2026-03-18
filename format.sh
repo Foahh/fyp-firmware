03d42aab3513cde733f0b86193a1ee32c576292c
Foahh <160589889+Foahh@users.noreply.github.com>
2026-03-18 12:50:01 +0800

    feat: update formatting configuration and script for improved code style consistency

--- format.sh
+++ format.sh
@@ -1,3 +1,6 @@
 #!/bin/bash
 
-find . -path ./Libraries -prune -o \( -name "*.c" -o -name "*.h" \) -print | xargs clang-format -i
+find ./Appli ./FSBL \( -name "*.c" -o -name "*.h" -o -name "*.cpp" -o -name "*.hpp" \) \
+  -not -path "*/build/*" \
+  -not -path "*/.cache/*" \
+  -print | xargs clang-format -i
