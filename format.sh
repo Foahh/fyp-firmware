8306b8d4b6d2b21e62e07b72012ae9eb8745c31e
Foahh <160589889+Foahh@users.noreply.github.com>
2026-03-19 18:54:08 +0800

    chore: remove unused format.sh

--- format.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-find ./Appli ./FSBL \( -name "*.c" -o -name "*.h" -o -name "*.cpp" -o -name "*.hpp" \) \
-  -not -path "*/build/*" \
-  -not -path "*/.cache/*" \
-  -not -path "*/Proto/nanopb/*" \
-  -print | xargs clang-format -i
